9cc0379fdfedb0b7360bb1618a50efd2168540d2
Jin <hi@oyjz.cn>
2023-01-03 09:51:00 +0800

    license check

---
+++ go.mod
@@ -0,0 +1,5 @@
+module license
+
+go 1.19
+
+require github.com/wenzhenxi/gorsa v0.0.0-20220418014903-15feec0f05a6 // indirect
---
+++ license_check.go
@@ -0,0 +1,82 @@
+package license
+
+import (
+	"crypto/md5"
+	"encoding/base64"
+	"errors"
+	"fmt"
+	"net"
+	"os"
+
+	"github.com/wenzhenxi/gorsa"
+)
+
+// 公钥
+var publicKey = `-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA3J+24OIkIYajQ/xRgG5m
+dvCcBkh1wwDy6UnMvxW42UT0TA0nENGjmHmIpkyxUhFMC9f/KHiskuoSURrhMAuz
+bcKE/aEmNFfNThm4XNLNo43F7/dROMzB2nXAcRJVjhFiaAdQ1s8vd4grN5fJSVKc
+H/4hDIxiN37a6QqyiQCB9+erRSMV5PEnA57PwlOr3+YSJafW55F73pt0XRk6kfAG
+Eiy8cugCG294Mc/SX/plRz4eCrjyGo1r4bWw7WxUB/Rt1blALJpLsFaN0M/9vmOM
+osw6y4SgljETZO5Hd7rQHDUipXd8g/8l5SoSoV1tNubH8v03dlAbDPZnG+c+Le+c
+2wIDAQAB
+-----END PUBLIC KEY-----`
+
+// GetID 获取机器码
+func GetID() string {
+	macStr := GetMac()
+
+	id := fmt.Sprintf("%x", md5.Sum([]byte(macStr)))
+	return id
+}
+
+// CheckLicense 校验授权
+func CheckLicense() {
+	if err := gorsa.RSA.SetPublicKey(publicKey); err != nil {
+		panic(err)
+	}
+
+	// 读取license
+	data, err := os.ReadFile("./config/license")
+	if err != nil {
+		panic(err)
+	}
+
+	// base64解码
+	data1, err := base64.StdEncoding.DecodeString(string(data))
+	if err != nil {
+		panic(err)
+	}
+
+	// rsa解密
+	id, err := gorsa.RSA.PubKeyDECRYPT(data1)
+	if err != nil {
+		panic(err)
+	}
+	if GetID() != string(id) {
+		panic(errors.New("invalid license"))
+	}
+}
+
+// GetMac 获取Mac
+func GetMac() (macAdds string) {
+	netInterfaces, err := net.Interfaces()
+	if err != nil {
+		fmt.Printf("fail to get net interfaces: %v", err)
+		return macAdds
+	}
+
+	for _, netInterface := range netInterfaces {
+		macAddr := netInterface.HardwareAddr.String()
+		if len(macAddr) == 0 {
+			continue
+		}
+
+		if len(macAdds) > 0 {
+			macAdds = macAdds + "," + macAddr
+		} else {
+			macAdds = macAddr
+		}
+	}
+	return macAdds
+}
